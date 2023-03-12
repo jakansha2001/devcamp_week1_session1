8fc90295e0b16b1df8e3d22784bfec8b45e556b6
jakansha2001 <jakansha2001@gmail.com>
2023-03-12 17:59:04 +0530

    hero animation

--- lib/detailsPage.dart
+++ lib/detailsPage.dart
@@ -1,22 +1,56 @@
 import 'package:devcamp_session1/models/data.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class DetailsPage extends StatefulWidget {
   final Data city;
-  DetailsPage({required this.city});
-
+  const DetailsPage({super.key, required this.city});
 
   @override
   State<DetailsPage> createState() => _DetailsPageState();
 }
+
+
 
 class _DetailsPageState extends State<DetailsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.city.cityName,)
+        title: Text(
+          widget.city.cityName,
+        ),
+      ),
+      body: Column(
+        children: [
+          /// The source and destination widget should be wrapped with Hero widget. 
+          /// Hero widget takes a tag as a parameter which should be unique. 
+          /// The source and destination should have the same tag value
+          Hero(
+            tag: widget.city.cityName, //London
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.network(
+                  widget.city.cityImage,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Text(
+              widget.city.cityInfo,
+              textAlign: TextAlign.justify,
+              style: const TextStyle(
+                fontSize: 20,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:devcamp_session1/models/data.dart';
+import 'package:devcamp_session1/stack_page.dart';
 import 'package:flutter/material.dart';
 
 import 'detailsPage.dart';
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       home: const Home(),
     );
@@ -38,17 +39,27 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(
+          Icons.place,
+        ),
+      ),
       appBar: AppBar(
-        backgroundColor: Colors.red.shade800,
-        title: Text("Travel App"),
+        //ßbackgroundColor: Colors.red.shade800,
+        title: const Text("Travel App"),
       ),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            // Image.asset(
+            //   'assets/images/akansha.jpg',
+            // ),
             Image.network(
-                "https://img.freepik.com/premium-vector/travel-concept-vector-illustration-with-famous-sights-accessories_95169-98.jpg?w=1800"),
-            SizedBox(
+              "https://img.freepik.com/premium-vector/travel-concept-vector-illustration-with-famous-sights-accessories_95169-98.jpg?w=1800",
+            ),
+            const SizedBox(
               height: 20,
             ),
             Row(
@@ -64,10 +75,11 @@ class Home extends StatelessWidget {
                       color: Colors.red.shade800,
                     ),
                     child: const Center(
-                        child: Text(
-                          "Attraction",
-                          style: TextStyle(fontSize: 18, color: Colors.white),
-                        )),
+                      child: Text(
+                        "Attractions",
+                        style: TextStyle(fontSize: 18, color: Colors.white),
+                      ),
+                    ),
                   ),
                 ),
                 Padding(
@@ -80,10 +92,11 @@ class Home extends StatelessWidget {
                       color: Colors.red.shade800,
                     ),
                     child: const Center(
-                        child: Text(
-                          "Places",
-                          style: TextStyle(fontSize: 18, color: Colors.white),
-                        )),
+                      child: Text(
+                        "Places",
+                        style: TextStyle(fontSize: 18, color: Colors.white),
+                      ),
+                    ),
                   ),
                 ),
                 Padding(
@@ -96,67 +109,111 @@ class Home extends StatelessWidget {
                       color: Colors.red.shade800,
                     ),
                     child: const Center(
-                        child: Text(
-                          "Hotels",
-                          style: TextStyle(fontSize: 18, color: Colors.white),
-                        )),
+                      child: Text(
+                        "Hotels",
+                        style: TextStyle(fontSize: 18, color: Colors.white),
+                      ),
+                    ),
                   ),
                 )
               ],
             ),
+
             ///ListView.builder example
             // Container(
             //   height: 80,
             //   child: ListView.builder(
-            //     scrollDirection: Axis.horizontal,
-            //     itemCount: 3,
-            //       itemBuilder: (BuildContext context,index){
-            //       return Padding(
-            //         padding: const EdgeInsets.all(8.0),
-            //         child: Container(
-            //           width: 120,
-            //
-            //
-            //           decoration: BoxDecoration(
-            //             borderRadius: BorderRadius.circular(10),
-            //             color: Colors.red.shade800,
+            //       scrollDirection: Axis.horizontal,
+            //       itemCount: 4,
+            //       itemBuilder: (BuildContext context, index) {
+            //         return Padding(
+            //           padding: const EdgeInsets.all(8.0),
+            //           child: Container(
+            //             width: 120,
+            //             decoration: BoxDecoration(
+            //               borderRadius: BorderRadius.circular(10),
+            //               color: Colors.yellow.shade800,
+            //             ),
+            //             child: const Center(
+            //                 child: Text(
+            //               "Attraction",
+            //               style: TextStyle(
+            //                 fontSize: 18,
+            //                 color: Colors.white,
+            //               ),
+            //             )),
             //           ),
-            //           child: const Center(
-            //               child: Text(
-            //                 "Attraction",
-            //                 style: TextStyle(fontSize: 18, color: Colors.white),
-            //               )),
-            //         ),
-            //       );
+            //         );
             //       }),
             // ),
             const Padding(
-              padding: EdgeInsets.only(left: 15,top: 20),
-              child: Text("Popular Destination",style: TextStyle(fontSize: 20),),
+              padding: EdgeInsets.only(left: 15, top: 20),
+              child: Text(
+                "Popular Destinations",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'Tilt Prism',
+                ),
+              ),
             ),
             GridView.builder(
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                itemCount: datalist.length,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2), itemBuilder: (BuildContext context,index){
-              return InkWell(
-                onTap: (){
-                 Navigator.push(context, MaterialPageRoute(builder: (_)=>DetailsPage(city:datalist[index])));
-                },
-                child: Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: Container(
-                    color: Colors.red.shade800,
-                    child: Column(
-                      children: [
-                        Image.network(datalist[index].cityImage.toString()),
-                        Center(child: Text(datalist[index].cityName.toString(),style: TextStyle(color: Colors.white,fontSize: 20),)),
-                      ],
+              /// By setting [shrinkWrap] true,
+              /// we tell gridview to just occupy minimum possible space
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: datalist.length,
+
+              /// [gridDelegate] defines how you can lay down your children
+              /// on main and cross axis
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+              ),
+              itemBuilder: (BuildContext context, index) {
+                return InkWell(
+                  
+                  /// This is used when we have to capture any user interaction
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => DetailsPage(
+                          city: datalist[index],
+                        ),
+                      ),
+                    );
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Container(
+                      color: Colors.red.shade800,
+                      child: Column(
+                        children: [
+                          Hero(
+                            tag: datalist[index].cityName, //London
+                            child: Image.network(
+                              datalist[index].cityImage,
+                              // datalist[0].cityImage
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Center(
+                            child: Text(
+                              datalist[index].cityName,
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              );
-            })
+                );
+              },
+            )
           ],
         ),
       ),
